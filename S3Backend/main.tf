a152b7572247c9ab1c606bea768c90d5db610b05
Harshit Baliyan <harshit.baliyan.cseiot.2020@miet.ac.in>
2025-03-25 17:37:42 +0530

    VPC Configurations

--- S3Backend/main.tf
+++ S3Backend/main.tf
@@ -7,3 +7,136 @@ resource "aws_instance" "my_ec2" {
     Name = "Terraform-EC2"
   }
 }
+
+resource "aws_vpc" "secure_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "secureVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.secure_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+
+  tags = { Name = "PublicSubnet" }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.secure_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.secure_vpc.id
+
+  tags = {
+    Name = "SecureIGW"
+  }
+}
+
+resource "aws_eip" "nat_eip" {}
+
+resource "aws_nat_gateway" "nat_gw" {
+  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.nat_eip.id
+  tags = {
+    Name = "SecureNAT"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.secure_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.secure_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+
+}
+
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.secure_vpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "PublicSG"
+  }
+
+}
+
+resource "aws_security_group" "private_sg" {
+  vpc_id = aws_vpc.secure_vpc.id
+
+  ingress {
+    description = "Allow Internal communication"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "privateSG"
+  }
+}
